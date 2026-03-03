8d4d1a178227ede5e1e7060cd902783fda564683
Charlie Mer Libatod <malinaocharlie74@gmail.com>
2026-03-03 22:51:19 +0800

    first commit

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,6 +5,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
   <meta name="csrf-token" content="{{ csrf_token() }}">
   <title>@yield('title', 'Charlie Mer Libatod - Aspiring Software Engineer')</title>
+  <link rel="icon" type="image/x-icon" href="{{ asset('images/chaalogo.png') }}">
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
   <link href="https://fonts.googleapis.com/css2?family=DM+Sans:opsz,wght@9..40,300;9..40,400;9..40,500;9..40,600&family=DM+Serif+Display&display=swap" rel="stylesheet"/>
